c160f74104ac586e9553d040a4fc7a5dda3dc1ea
Mauro Gonzalo Gutierrez <maurog7gool@hotmail.com>
2022-03-07 00:36:34 -0300

    empezando retos

---
+++ bash.js
@@ -0,0 +1,34 @@
+// console.log(Object.keys(process));
+
+//------------------------------------------------------------
+
+// //TAREA 2:
+// // Output un prompt
+// process.stdout.write("prompt > ");
+// // El evento stdin 'data' se dispara cuando el user escribe una línea
+// process.stdin.on("data", function (data) {
+//   var cmd = data.toString().trim(); // remueve la nueva línea
+//   process.stdout.write("You typed: " + cmd);
+//   process.stdout.write("\nprompt > ");
+// })
+
+//------------------------------------------------------------
+
+const commands = require("./commands/index.js");
+
+// Output un prompt
+process.stdout.write("prompt > ");
+// El evento stdin 'data' se dispara cuando el user escribe una línea
+process.stdin.on("data", function (data) {
+  const cmd = data.toString().trim(); // remueve la nueva línea
+  if (cmd === "date") {
+    commands[cmd]();
+  }
+  if (cmd === "pwd") {
+    commands[cmd]();
+  }
+  if (cmd === "ls") {
+    commands[cmd]();
+  }
+  process.stdout.write("\nprompt > ");
+});
---
+++ commands/index.js
@@ -0,0 +1,25 @@
+var fs = require("fs");
+
+const pwd = () => {
+  process.stdout.write(process.cwd());
+};
+
+const date = () => {
+  process.stdout.write(Date());
+};
+
+const ls = () => {
+  fs.readdir(".", (err, files) => {
+    if (err) throw err;
+    files.forEach((file) => {
+      process.stdout.write(file.toString() + "\n"); //"\n" significa un salto de linea
+    });
+    process.stdout.write("prompt > ");
+  });
+};
+
+module.exports = {
+  pwd,
+  date,
+  ls,
+};
